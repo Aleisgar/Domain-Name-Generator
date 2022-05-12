187abea5e249eb5835befe2314dfd428516171ed
Aleisgar <aleisgar@gmail.com>
2022-05-12 08:10:36 +0000

    upload

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "my", "an"];
+  let adj = ["great", "big", "blue", "random"];
+  let noun = ["jogger", "racoon", "cat", "place"];
+
+  function domains() {
+    for (let i of pronoun) {
+      for (let j of adj) {
+        for (let k of noun) {
+          console.log(i + j + k + ".com");
+        }
+      }
+    }
+  }
+
+  document.querySelector("#dominio").innerHTML = domains();
 };
